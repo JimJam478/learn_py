012e8a5c3f062aeb624b61447aee2ab940bc4270
JimJam47 <allensreebinoj2010@gmail.com>
2023-12-12 12:16:30 +0530

    Exercise 20

--- ex20.py
+++ ex20.py
@@ -4,13 +4,13 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):
-print(f.read())
+    print(f.read())
 
 def rewind(f):
-f.seek(0)
+    f.seek(0)
 
 def print_a_line(line_count, f):
-print(line_count, f.readline())
+    print(line_count, f.readline())
 
 current_file = open(input_file)
 
@@ -24,7 +24,7 @@ rewind(current_file)
 
 print("Let's print three lines:")
 
-2urrent_line = 1
+current_line = 1
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
